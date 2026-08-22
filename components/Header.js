6cd2e3b3cdf1c8d7a742713eeb2703b24d6d5327
joeywong800 <joeywong800@gmail.com>
2026-08-22 15:09:21 +0200

    Change header animation opacity to 0

--- components/Header.js
+++ components/Header.js
@@ -124,7 +124,7 @@ export default function Header() {
     <>
       <motion.header
         initial={{ y: -80, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
+        animate={{ y: 0, opacity: 0 }}
         transition={{ duration: 0.45, ease: "easeOut" }}
         style={{
           background: "linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0));",
