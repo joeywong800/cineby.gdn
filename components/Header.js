bb46dce9af21aadfed139ce6ed1df9110e3c6fac
joeywong800 <joeywong800@gmail.com>
2026-08-23 16:34:11 +0200

    Fix style syntax in Header component

--- components/Header.js
+++ components/Header.js
@@ -130,8 +130,8 @@ export default function Header() {
           background: "background: linear-gradient(rgba(0, 0, 0, 0.8) 5%, transparent 100%);",
           position: "absolute",
           top: 0,
-          left: 50%,
-          transform: translateX(-50%),
+          left: "50%",
+          transform: "translateX(-50%)",
           zIndex: 1000,
           backdropFil: "blur(20px)",
         }}
