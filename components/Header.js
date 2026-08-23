4809a97f1a2af8214a8e5edbe61809dc2fbddce6
joeywong800 <joeywong800@gmail.com>
2026-08-23 17:27:57 +0200

    Change header position from sticky to absolute

--- components/Header.js
+++ components/Header.js
@@ -129,8 +129,11 @@ export default function Header() {
         style={{
           background: "background: linear-gradient(rgba(0, 0, 0, 0.8) 5%, transparent 100%);",
           display: "flex",
-          position: "sticky",
+          width: "100%",
+          position: "absolute",
           top: 0,
+          left: "auto",
+          right: "auto",
           zIndex: 1000
         }}
       >
