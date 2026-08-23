f9ae6eacd4b76dab6c67ddcdee6bfc9949b6c31b
joeywong800 <joeywong800@gmail.com>
2026-08-23 15:05:46 +0200

    Fix Header style properties for better layout

--- components/Header.js
+++ components/Header.js
@@ -129,7 +129,8 @@ export default function Header() {
         style={{
           background: "background: linear-gradient(rgba(0, 0, 0, 0.8) 0%, transparent 100%);",
           position: "absolute",
-          margin: 0 auto,
+          display: "block",
+          margin: "0 auto",
           top: 0,
           zIndex: 1000,
           backdropFil: "blur(20px)",
